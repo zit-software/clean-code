4361c43ce567bf3a1665d86fc9e982b2942ed3ae
Kim Minh Thang <thangved02@gmail.com>
2024-10-09 21:25:55 +0700

    refactor: refactor `auth.service`

--- services/auth.service.js
+++ services/auth.service.js
@@ -1,48 +1,38 @@
 import User from '../models/user.js';
 
+const MIN_PASSWORD_LENGTH = 6;
+
 class AuthService {
   /**
    * Signs up a new user
-   * @param {object} signup_payload
-   * @param {string} signup_payload.username
-   * @param {string} signup_payload.password
-   * @param {string} signup_payload.confirmPassword
+   * @param {object} signUpPayload
+   * @param {string} signUpPayload.username
+   * @param {string} signUpPayload.password
+   * @param {string} signUpPayload.confirmPassword
    */
-  async signUp(signup_payload) {
-    if (signup_payload.username) {
-      if (signup_payload.password) {
-        const existingUser = await User.findOne({
-          where: {username: signup_payload.username},
-          raw: true,
-        });
+  async signUp(signUpPayload) {
+    if (!signUpPayload.username) throw new Error('Username is required');
+    if (!signUpPayload.password) throw new Error('Password is required');
 
-        if (!existingUser) {
-          if (signup_payload.password.length >= 6) {
-            if (signup_payload.password === signup_payload.confirmPassword) {
-              return await User.create(
-                {
-                  username: signup_payload.username,
+    if (signUpPayload.password.length < MIN_PASSWORD_LENGTH)
+      throw new Error(`Password must be at least ${MIN_PASSWORD_LENGTH} characters long`);
 
-                  // THIS IS JUST FOR DEMO PURPOSES, IN A REAL APP YOU SHOULD HASH THE PASSWORD BEFORE SAVING IT TO THE DATABASE
-                  password: signup_payload.password,
-                },
-                {raw: true}
-              );
-            } else {
-              throw new Error('Password and confirm password do not match');
-            }
-          } else {
-            throw new Error('Password must be at least 6 characters long');
-          }
-        } else {
-          throw new Error('User already exists');
-        }
-      } else {
-        throw new Error('Password is required');
-      }
-    } else {
-      throw new Error('Username is required');
-    }
+    if (signUpPayload.password !== signUpPayload.confirmPassword)
+      throw new Error('Password and confirm password do not match');
+
+    const existingUser = await User.findOne({
+      where: {username: signUpPayload.username},
+      raw: true,
+    });
+    if (existingUser) throw new Error('User already exists');
+
+    return User.create(
+      {
+        username: signUpPayload.username,
+        password: signUpPayload.password,
+      },
+      {raw: true}
+    );
   }
 
   /**
@@ -52,29 +42,17 @@ class AuthService {
    * @param {string} loginPayload.password
    */
   async signIn(loginPayload) {
-    if (loginPayload.username) {
-      if (loginPayload.password) {
-        const user = await User.findOne({
-          where: {username: loginPayload.username},
-          raw: true,
-        });
+    if (!loginPayload.username) throw new Error('Username is required');
+    if (!loginPayload.password) throw new Error('Password is required');
 
-        if (user) {
-          // THIS IS JUST FOR DEMO PURPOSES, IN A REAL APP YOU SHOULD HASH THE PASSWORD BEFORE COMPARING IT
-          if (user.password === loginPayload.password) {
-            return user;
-          } else {
-            throw new Error('Password is incorrect');
-          }
-        } else {
-          throw new Error('User not found');
-        }
-      } else {
-        throw new Error('Password is required');
-      }
-    } else {
-      throw new Error('Username is required');
-    }
+    const user = await User.findOne({
+      where: {username: loginPayload.username},
+      raw: true,
+    });
+    if (!user) throw new Error('User not found');
+
+    if (user.password !== loginPayload.password) throw new Error('Password is incorrect');
+    return user;
   }
 }
 
